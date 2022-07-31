0886cf592c40a458ac45d1dd36f166124f866839
Andy <avadnais@oswego.edu>
2022-07-30 22:45:26 -0600

    styling add and edit campground

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
+const { places, descriptors, images } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -25,7 +25,10 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: sample(images),
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque non tellus orci ac auctor augue mauris augue neque. Tortor id aliquet lectus proin. Faucibus turpis in eu mi bibendum neque egestas. Sed libero enim sed faucibus turpis in eu. Eget nullam non nisi est sit. Vestibulum mattis ullamcorper velit sed ullamcorper morbi. Arcu risus quis varius quam. Consectetur a erat nam at lectus urna duis. Arcu dictum varius duis at consectetur lorem donec massa.',
+            price: Math.floor(Math.random() * 20) + 10
         })
         await camp.save();
     }
--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -1,3 +1,5 @@
+const { model } = require("mongoose")
+
 module.exports.descriptors = [
     'Forest',
     'Ancient',
@@ -43,3 +45,14 @@ module.exports.places = [
     'Hollow'
 ]
 
+module.exports.images = [
+    'https://images.unsplash.com/photo-1518602164578-cd0074062767?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1507371341162-763b5e419408?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1339&q=80',
+    'https://images.unsplash.com/photo-1507584359040-f44a16355689?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+    'https://images.unsplash.com/photo-1504673627366-ed44f724fadb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+    'https://images.unsplash.com/photo-1503516459261-40c66117780a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
+    'https://images.unsplash.com/photo-1503093928907-326ec3f06115?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    'https://images.unsplash.com/photo-1502886227923-92453428325f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=716&q=80',
+    'https://images.unsplash.com/photo-1454614146802-84004a53428d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+    'https://images.unsplash.com/photo-1500370414137-9201565cf099?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+]
